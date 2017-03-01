47c21adc941afd67830469ae8e4734910a597316
juanma0474 <juanma0474@hotmail.com>
2017-03-01 11:20:23 -0500

    Actualizacion

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import './index.css';
 
 const config = {
-  apiKey: "AIzaSyDitW7uHb6tE6qtbLgS_1d9tajYWDjdwxY",
-  authDomain: "pseudogram-ed48c.firebaseapp.com",
-  databaseURL: "https://pseudogram-ed48c.firebaseio.com",
-  storageBucket: "pseudogram-ed48c.appspot.com",
-  messagingSenderId: "887707537740"
+  apiKey: "INGRESAR KEY",
+  authDomain: "INGRESAR DOMINIO",
+  databaseURL: "INGRESAR URL",
+  storageBucket: "INGRESAR STORAGE",
+  messagingSenderId: "INGRESAR ID"
 };
 
 firebase.initializeApp(config);
