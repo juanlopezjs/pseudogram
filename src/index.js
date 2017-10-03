fa1a61b43067139f02f30451eb378f4063797841
jmlb1905 <juanm_lopez@coomeva.com.co>
2017-10-03 08:14:27 -0500

    config firebase

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ import './App.css';
 
 
 const config = {
-
+    apiKey: "AIzaSyDitW7uHb6tE6qtbLgS_1d9tajYWDjdwxY",
+    authDomain: "pseudogram-ed48c.firebaseapp.com",
+    databaseURL: "https://pseudogram-ed48c.firebaseio.com",
+    storageBucket: "pseudogram-ed48c.appspot.com",
+    messagingSenderId: "887707537740"
 };
 
 const history = createBrowserHistory();
